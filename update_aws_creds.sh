ee6733dfea31c93c845d550f6ed68e7a60b0bfd0
Mageshwaran <mageshwaran960@gmail.com>
2025-03-06 10:51:15 +0000

    lastest messages

---
+++ update_aws_creds.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Fetch new temporary credentials for 4 hours (14400 seconds)
+CREDS_JSON=$(aws sts get-session-token --duration-seconds 14400)
+
+# Parse the credentials (ensure jq is installed: sudo apt-get install jq)
+ACCESS_KEY=$(echo "$CREDS_JSON" | jq -r '.Credentials.AccessKeyId')
+SECRET_KEY=$(echo "$CREDS_JSON" | jq -r '.Credentials.SecretAccessKey')
+SESSION_TOKEN=$(echo "$CREDS_JSON" | jq -r '.Credentials.SessionToken')
+
+# Write the credentials to the default AWS credentials file
+cat > ~/.aws/credentials <<EOF
+[default]
+aws_access_key_id = ${ACCESS_KEY}
+aws_secret_access_key = ${SECRET_KEY}
+aws_session_token = ${SESSION_TOKEN}
+EOF
+
+echo "AWS credentials updated at $(date)"
+
